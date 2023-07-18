67e56aff0506ffb0a3911b35c76ddd1e3823fc80
iTech Reset Electronics <itechresetelectronics@iTechs-MacBook-Air.local>
2023-07-17 20:40:50 -0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "../styles/globals.css";
+//import "../styles/globals.css";
+import {ChakraProvider} from "@chakra-ui/react";
+import { Sepolia } from "@thirdweb-dev/chains";
+import {Navbar} from "../components/Navbar";
+
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
 
+ 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    
+    <ThirdwebProvider activeChain={Sepolia}>
+      <ChakraProvider>
+        <Navbar/>
       <Component {...pageProps} />
+      </ChakraProvider>
     </ThirdwebProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,21 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import NextLink from 'next/link';
+import { Container, Heading, Flex, Stack, Button } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
+    <Container maxW={"1200px"}>
+      <Flex h={"80vh"} alignItems={"center"} 
+      justifyContent={"center"}>
+        <Stack>
+          <Heading>Marketplace</Heading>
+          <Button
+          as={NextLink} href='/buy'>Shop Sanctum NFTs</Button>
+        </Stack>
+      </Flex>
+    </Container>
   );
 };
 
